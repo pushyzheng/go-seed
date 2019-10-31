47b9fd872b5b1366785c55bc6caa7ade0e184420
Pushy <1437876073@qq.com>
2019-10-31 21:08:14 +0800

    fix: cannot throws a ordinary error

--- web.go
+++ web.go
@@ -1,7 +1,6 @@
 package goseed
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,19 +12,28 @@ type HttpError struct {
 }
 
 func (err HttpError) Error() string {
-	return fmt.Sprintf("The error code is %d, message is %s", err.Code, err.Msg)
+	return err.Msg
 }
 
 // catch panic error and return error response
 func catch(ctx *gin.Context) {
 	if e := recover(); e != nil {
-		msg := e.(HttpError).Msg
-		code := e.(HttpError).Code
-		resp := response{
-			"code":    code,
-			"message": msg,
-			"data":    nil,
+		var code int
+		var msg string
+
+		switch e.(type) {
+		// 如果是自定义 HttpError ，则获取状态码和错误信息
+		case HttpError:
+			msg = e.(HttpError).Msg
+			code = e.(HttpError).Code
+			break
+		// 如果是普通的 error， 则转换为 error
+		case error:
+			msg = e.(error).Error()
+			code = 500
+			break
 		}
+		resp := getResponse(code, msg, nil)
 		ctx.JSON(code, resp)
 	}
 }
@@ -35,11 +43,31 @@ func WrapHandle(f func(ctx *gin.Context) interface{}) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		defer catch(ctx)
 		result := f(ctx)
-		resp := response{
-			"code":    200,
-			"message": nil,
-			"data":    result,
-		}
+		resp := getResponse(200, nil, result)
 		ctx.JSON(200, resp)
+	}
+}
+
+func AbortErr(ctx *gin.Context, code int, msg string) {
+	resp := getResponse(code, msg, nil)
+	ctx.AbortWithStatusJSON(code, resp)
+}
+
+func ThrowsErr(code int, err error) {
+	panic(HttpError{Code: code, Msg: err.Error()})
+}
+
+func ParseJSON(ctx *gin.Context, obj interface{}) {
+	err := ctx.BindJSON(obj)
+	if err != nil {
+		ThrowsErr(400, err)
+	}
+}
+
+func getResponse(code int, msg interface{}, data interface{}) response {
+	return response{
+		"code":    code,
+		"message": msg,
+		"data":    data,
 	}
 }
